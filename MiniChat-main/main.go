8b72fe2ce74efdb277218bf21ec900df1410fc3f
LianHK123 <lianhengkai@163.com>
2025-04-25 16:47:57 +0800

    添加MySQL数据库
    添加MySQL数据库存储user、room、message...

---
+++ MiniChat-main/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+	"minichat/config"
+	"minichat/conversation"
+	"minichat/server"
+	"minichat/util"
+	"net/http"
+)
+
+//go:embed static
+var DirStatic embed.FS
+
+//go:embed templates/*
+var DirTemplate embed.FS
+
+func main() {
+
+	http.HandleFunc("/precheck", server.PreCheck)
+	http.HandleFunc("/ws", server.HandleWs)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		server.HandleFiles(writer, request, DirTemplate)
+	})
+	fs := http.FileServer(http.FS(DirStatic))
+	http.Handle("/static/", fs)
+
+	go conversation.Manager.Start()
+
+	configVal := config.ParseConfig("config.yaml")
+
+	util.InitDB()
+
+	log.Printf("\n\n********************************\nChat server is running at %d !\n********************************\n\n", configVal.Port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", configVal.Port), nil)
+	if err != nil {
+		fmt.Printf("Server start fail, error is: [ %+v ]", err)
+		return
+	}
+}
